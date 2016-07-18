ec78d87997a13940f80ebf8c6d4e7220fd8d52f0
jeffreyfeng12 <jfeng12@terpmail.umd.edu>
2016-07-18 12:17:18 -0400

    navigation drawer as main

--- openxc-starter-master/app/src/main/java/com/openxc/openxcstarter/MainActivity.java
+++ openxc-starter-master/app/src/main/java/com/openxc/openxcstarter/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
 import com.openxcplatform.openxcstarter.R;
 
 public class MainActivity extends Activity
