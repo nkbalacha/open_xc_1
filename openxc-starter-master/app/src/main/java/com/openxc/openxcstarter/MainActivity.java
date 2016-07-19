98a38dfb337ff35ed831e4e5cdfaa1ab2bacbe28
jeffreyfeng12 <jfeng12@terpmail.umd.edu>
2016-07-19 12:12:00 -0400

    formatting done
    nav drawer completed, no autohide on the transit page

--- openxc-starter-master/app/src/main/java/com/openxc/openxcstarter/MainActivity.java
+++ openxc-starter-master/app/src/main/java/com/openxc/openxcstarter/MainActivity.java
@@ -5,9 +5,14 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,23 +21,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.openxc.VehicleManager;
+import com.openxc.measurements.EngineSpeed;
+import com.openxc.measurements.FuelConsumed;
+import com.openxc.measurements.Measurement;
+import com.openxc.measurements.SteeringWheelAngle;
+import com.openxc.measurements.VehicleSpeed;
+import com.openxcplatform.openxcstarter.InTransitActivity;
 import com.openxcplatform.openxcstarter.R;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+    private static final String TAG = "MainActivity";
+
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
+    private VehicleManager mVehicleManager;
+    private TextView mEngineSpeedView;
+    private TextView mVehicleSpeedView;
+    private TextView mSteeringAngleView;
+    private TextView mFuelConsumedView;
+
+
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +70,132 @@ public class MainActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
+        mEngineSpeedView = (TextView) findViewById(R. id.engine_speed);
+        mVehicleSpeedView = (TextView) findViewById(R.id.vehicle_speed);
+        mSteeringAngleView = (TextView) findViewById(R.id.steering_angle);
+        mFuelConsumedView = (TextView) findViewById(R.id.fuel_consumption);
+
+        goToTrip();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // When the activity goes into the background or exits, we want to make
+        // sure to unbind from the service to avoid leaking memory
+        if(mVehicleManager != null) {
+            Log.i(TAG, "Unbinding from Vehicle Manager");
+            // Remember to remove your listeners, in typical Android
+            // fashion.
+            mVehicleManager.removeListener(EngineSpeed.class,
+                    mEngineSpeedListener);
+            mVehicleManager.removeListener(VehicleSpeed.class,
+                    mVSpeedListener);
+            mVehicleManager.removeListener(SteeringWheelAngle.class,
+                    mWheelAngleListener);
+            mVehicleManager.removeListener(FuelConsumed.class,
+                    mFuelListener);
+
+            unbindService(mConnection);
+            mVehicleManager = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mVehicleManager == null) {
+            Intent intent = new Intent(this, VehicleManager.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    EngineSpeed.Listener mEngineSpeedListener = new EngineSpeed.Listener() {
+        @Override
+        public void receive(Measurement measurement) {
+            final EngineSpeed speed = (EngineSpeed) measurement;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    mEngineSpeedView.setText("Engine speed (RPM): "
+                            + speed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    VehicleSpeed.Listener mVSpeedListener = new VehicleSpeed.Listener() {
+        public void receive(Measurement measurement) {
+            final VehicleSpeed speed = (VehicleSpeed) measurement;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mVehicleSpeedView.setText("Vehicle speed (KM/H): "
+                            + speed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    SteeringWheelAngle.Listener mWheelAngleListener = new SteeringWheelAngle.Listener() {
+        public void receive(Measurement measurement) {
+            final SteeringWheelAngle angle = (SteeringWheelAngle) measurement;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mSteeringAngleView.setText("Steering Wheel Angle: "
+                            + angle.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    FuelConsumed.Listener mFuelListener = new FuelConsumed.Listener() {
+        public void receive(Measurement measurement) {
+            final FuelConsumed fuel = (FuelConsumed) measurement;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mFuelConsumedView.setText("Fuel Consumed: "
+                            + fuel.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            Log.i(TAG, "Bound to VehicleManager");
+            mVehicleManager = ((VehicleManager.VehicleBinder) service)
+                    .getService();
+
+            mVehicleManager.addListener(EngineSpeed.class, mEngineSpeedListener);
+            mVehicleManager.addListener(VehicleSpeed.class, mVSpeedListener);
+            mVehicleManager.addListener(SteeringWheelAngle.class, mWheelAngleListener);
+            mVehicleManager.addListener(FuelConsumed.class, mFuelListener);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            Log.w(TAG, "VehicleManager Service  disconnected unexpectedly");
+            mVehicleManager = null;
+        }
+    };
+
+    public Button SmartTripButton;
+    public void goToTrip() {
+        SmartTripButton = (Button)findViewById(R.id.but_smartTrip);
+
+        SmartTripButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent changePage = new Intent(MainActivity.this, InTransitActivity.class);
+
+                startActivity(changePage);
+            }
+        });
     }
 
     @Override
