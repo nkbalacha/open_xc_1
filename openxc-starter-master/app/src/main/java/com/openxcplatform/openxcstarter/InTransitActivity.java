f78123e89fa9cbe96aa5e6bbe8d63606be5a9dc2
spencersokoloski <spencer.sokoloski@gmail.com>
2016-07-25 11:34:30 -0400

    idk my local github is adding all these files
    I have no idea what they do

--- openxc-starter-master/app/src/main/java/com/openxcplatform/openxcstarter/InTransitActivity.java
+++ openxc-starter-master/app/src/main/java/com/openxcplatform/openxcstarter/InTransitActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.icu.util.Measure;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -19,6 +20,9 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.openxc.VehicleManager;
 import com.openxc.measurements.EngineSpeed;
 import com.openxc.measurements.Measurement;
@@ -31,6 +35,10 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Defines the actions that occur as an Activity while the user is driving,
+ * and OpenXC is reading data.
+ */
 public class InTransitActivity extends Activity {
     private static final String TAG = "InTransitActivity";
 
@@ -38,6 +46,7 @@ public class InTransitActivity extends Activity {
     private TextView mBackground;
     private int red = 0;
     private int green = 255;
+    /* red = place, green = 255 - place */
     private int place = 0;
 
     // OpenXC data
@@ -53,6 +62,12 @@ public class InTransitActivity extends Activity {
     private ArrayList<Double> totalLong = new ArrayList<>();
     private ArrayList<Double> ruleLat = new ArrayList<>();
     private ArrayList<Double> ruleLong = new ArrayList<>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,15 +76,13 @@ public class InTransitActivity extends Activity {
         setContentView(R.layout.activity_in_transit);
 
         // This is no longer necessary
-         mBackground = (TextView)findViewById(R.id.fullscreen_content);
+        mBackground = (TextView) findViewById(R.id.fullscreen_content);
 
         // constantly changing from red to green
         Timer myTimer = new Timer();
-        myTimer.schedule(new TimerTask()
-        {
+        myTimer.schedule(new TimerTask() {
             @Override
-            public void run()
-            {
+            public void run() {
                 redToGreen();
             }
         }, 0, 500);
@@ -78,12 +91,15 @@ public class InTransitActivity extends Activity {
         goToReview();
         testRule();
         getLocation();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if(mVehicleManager != null) {
+        if (mVehicleManager != null) {
             Log.i(TAG, "Unbinding from Vehicle Manager");
             mVehicleManager.removeListener(EngineSpeed.class,
                     mEngineSpeedListener);
@@ -102,7 +118,7 @@ public class InTransitActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        if(mVehicleManager == null) {
+        if (mVehicleManager == null) {
             Intent intent = new Intent(this, VehicleManager.class);
             bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
         }
@@ -147,7 +163,7 @@ public class InTransitActivity extends Activity {
         }
     };
 
-    Latitude.Listener mLatListener = new Latitude.Listener(){
+    Latitude.Listener mLatListener = new Latitude.Listener() {
         @Override
         public void receive(Measurement measurement) {
             final Latitude lati = (Latitude) measurement;
@@ -201,8 +217,7 @@ public class InTransitActivity extends Activity {
     }
 
     // the timer!!
-    public void redToGreen()
-    {
+    public void redToGreen() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -222,8 +237,7 @@ public class InTransitActivity extends Activity {
         });
     }
 
-    public void rule()
-    {
+    public void rule() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -235,10 +249,11 @@ public class InTransitActivity extends Activity {
     }
 
     public Button MapReviewButton;
-    public void goToReview() {
-        MapReviewButton = (Button)findViewById(R.id.stop_button);
 
-        MapReviewButton.setOnClickListener(new View.OnClickListener(){
+    public void goToReview() {
+        MapReviewButton = (Button) findViewById(R.id.stop_button);
+
+        MapReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent transferMapData = new Intent(InTransitActivity.this, MapReviewActivity.class);
@@ -255,8 +270,9 @@ public class InTransitActivity extends Activity {
     }
 
     public Button TestButton;
+
     public void testRule() {
-        TestButton = (Button)findViewById(R.id.test_Button);
+        TestButton = (Button) findViewById(R.id.test_Button);
 
         TestButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -272,5 +288,45 @@ public class InTransitActivity extends Activity {
     public void getLocation() {
         totalLat.add(lat);
         totalLong.add(lng);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "InTransit Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.openxcplatform.openxcstarter/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "InTransit Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.openxcplatform.openxcstarter/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
