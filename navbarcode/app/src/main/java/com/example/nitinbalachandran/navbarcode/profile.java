908593f014672f96bf7ff0ffe58a5d3cce3a1953
nkbalacha <nitinkb2015@gmail.com>
2016-07-27 12:31:00 -0400

    test code

---
+++ navbarcode/app/src/main/java/com/example/nitinbalachandran/navbarcode/profile.java
@@ -0,0 +1,38 @@
+package com.example.nitinbalachandran.navbarcode;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link profile.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link profile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class profile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_profile, container,false);
+
+
+        return view;
+    }
+
+}
